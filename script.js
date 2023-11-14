4c604b57746fb4b7fc5b4b9a3438e398acd9ac88
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-14 12:39:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
 // your code here
+
+	let v=[0,1];
+
+	if(n==0)
+		return 0;
+	if(n==1)
+		return 1;
+
+	for(let i=2;i<=n;i++)
+		{
+			let sum=v[i-1]+v[i-2];
+			v.push(sum);
+		}
+
+	return v[n-1];
 }
 
 module.exports = fibonacci;
